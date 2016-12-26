b07e574a18e16b4ce2af94186624d62084f14fdc
yenhao <yenhao0218@gmail.com>
2016-12-26 20:43:00 +0800

    basic visualization of patient's post

---
+++ detect time/EmotionDetection.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import urllib
+
+
+class EmotionDetection(object):
+    """docstring for EmotionDetection"""
+    def __init__(self):
+        super(EmotionDetection, self).__init__()
+
+    #url = 'http://140.114.77.14:8080/webresources/jammin/emotion'
+    __url = 'http://192.168.2.30:8080/webresources/jammin/emotion'
+    __emo = None
+
+    def get_obj(self, text, lang = 'en'):
+        payload = {"text":text,"lang":lang}
+        r = requests.post(self.__url,  data = json.dumps(payload))
+        return r
+
+    def get_emotion(self, text, lang = 'en'):
+        r = self.get_obj(text, lang)
+        return r.json()["groups"]
+
+    def get_emotion_json(self, text, lang = 'en'):
+        r = self.get_obj(text, lang)
+        self.__emo = r.json()
+        return self.__emo
+
+    def is_ambigous(self, text, lang = 'en'):
+        r = self.get_obj(text, lang)
+        r = r.json()
+        
+        if r['ambiguous'] == 'no':
+            return False
+        else:
+            return True
+
+    def is_ambigous_json(self, r):
+        if r['ambiguous'] == 'no':
+            return False
+        else:
+            return True
