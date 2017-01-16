41a0d7f87bd293921cf6edb383b9ceccad759a77
yenhao <yenhao0218@gmail.com>
2017-01-16 20:56:42 +0800

    Implement Wordcloud and  show it

--- detect time/web/doctor_view/Hello.py
+++ detect time/web/doctor_view/Hello.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from collections import defaultdict
 import random
+from os import path
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import re
 
 app = Flask(__name__)
+
+# function to delete url
+def del_url(line):
+    return re.sub(r'https?:\/\/.*', "", line)
 
 # Initial
 def loadTweets():
@@ -65,9 +73,63 @@ def index():
     index = random.randrange(0, len(user_list)-1)
     return render_template("index.html", user = str(user_list[index]), user_info_dict = user_info_dict[str(user_list[index])], user_tweets_dict = user_tweets_dict[str(user_list[index])])
 
-@app.route("/initusr")
-def firstusr():
-    return render_template('result.html', result = dict)
+@app.route("/pevious")
+def previous():
+    user = request.form['user']
+    index = user_list.index(user)
+    return render_template("index.html", user = str(user_list[index]), user_info_dict = user_info_dict[str(user_list[index])], user_tweets_dict = user_tweets_dict[str(user_list[index])])
+
+@app.route("/getTweets", methods=['POST'])
+def returnTweets():
+    user = request.form['user']
+    start = request.form['start']
+    end = request.form['end']
+
+    #Generate user tweets html
+    html_content = '''
+        <div class="row">
+            <h3>Tweets from {} to {} </h3>
+        </div>
+        <br/>
+        <div class="list-group" style="padding-bottom:150px">
+        '''.format(start, end)
+    tweets_text = ''
+    for date in user_tweets_dict[user]:
+        if date >= int(start.replace('/','')) and date <= int(end.replace('/','')):
+            tweets = user_tweets_dict[user][date]
+            for tweet in tweets:
+                html_content += '''
+                    <a class="list-group-item">
+                        <h4 class="list-group-item-heading">{}</h4>
+                        <p class="list-group-item-text">{}</p>
+                    </a>
+                    '''.format(tweet[1],tweet[0])
+                tweets_text += del_url(tweet[1]) + ' '
+    html_content += '</div><center><a class="btn btn-lg btn-default" href="#head" role="button">Top</a></center><br/>'
+
+
+
+    # Generate a word cloud image
+    # wordcloud = WordCloud().generate(tweets_text)
+
+    # plt.imshow(wordcloud)
+    # plt.axis("off")
+
+    # lower max_font_size
+    wordcloud = WordCloud(max_font_size=40).generate(tweets_text)
+    plt.figure()
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    # plt.show()
+    fig = plt.gcf()
+    # fig.set_size_inches(18.5, 14.5, forward=True)
+    filename = user + '_' +start.replace('/','') + end.replace('/','') + '.png'
+    fig.savefig('static/img/wordcloud/' + filename, dpi=250)
+    plt.clf()
+
+    img_html = "<img src=\"/static/img/wordcloud/"+filename+"\" >"
+
+    return str(img_html) + '!@!@!' + str(html_content)
 
 
 
